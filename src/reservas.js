177158f7caebd97d5c876e5753317e1453af66e4
Maria Ozuna <mareozuna@gmail.com>
2025-10-24 20:06:39 -0300

    se creo el bread reservas

--- src/reservas.js
+++ src/reservas.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { router as v1UsuariosRutas } from './v1/rutas/usuariosRutas.js'; //importo rutas
 import { router as v1ServiciosRutas} from './v1/rutas/serviciosRutas.js'
-
+import { router as v1ReservasRutas } from './v1/rutas/reservasRutas.js';
 
 const app = express();
 
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.use('/api/v1/usuarios', v1UsuariosRutas); //cuando haya una consulta a usuarios la ruta que se va a usar es v1UsuariosRutas
 app.use('/api/v1/servicios', v1ServiciosRutas);//creamos la ruta servicios
-
+app.use('/api/v1/reservas', v1ReservasRutas);
 
 process.loadEnvFile();
 app.listen(process.env.PUERTO, () => {
