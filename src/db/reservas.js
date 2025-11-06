a2211e1e66f325e45c617496808c870a7197707e
facuundo <140212058+facuundo@users.noreply.github.com>
2025-11-06 20:26:15 -0300

    Update reservas.js

--- src/db/reservas.js
+++ src/db/reservas.js
@@ -1,59 +1,38 @@
 import { conexion } from "./conexion.js";
 
 export default class Reservas {
-  //Browse: Obtiene todas las reservas
-  buscarTodasLasReservas = async () => {
-    const sql = "SELECT * FROM reservas WHERE activo = 1";
-    const [reservas] = await conexion.execute(sql);
-    return reservas;
-  };
-
-  buscarPropias = async (usuario_id) => {
-    const sql = "SELECT * FROM reservas WHERE activo = 1 AND usuario_id = ?";
-    const [reservas] = await conexion.execute(sql, [usuario_id]);
-    return reservas;
-  };
-
-  //Read: Obtiene reserva por id
-  buscarReserva = async (reserva_id) => {
-    const sql = `SELECT * FROM reservas WHERE activo = 1 AND reserva_id = ?`;
-    const [reserva] = await conexion.execute(sql, [reserva_id]);
-    if (reserva.length === 0) {
-      return null;
-    }
-    return reserva[0];
-  };
-
-  //Add: Agrega nueva reserva
-  agregarReserva = async (reserva) => {
-    const {
-      fecha_reserva,
-      salon_id,
-      usuario_id,
-      turno_id,
-      foto_cumpleaniero,
-      tematica,
-      importe_salon,
-      importe_total,
-    } = reserva;
-    const sql = `INSERT INTO reservas (fecha_reserva, salon_id, usuario_id, turno_id, foto_cumpleaniero, tematica, importe_salon, importe_total) VALUES(?,?,?,?,?,?,?,?)`;
-
-    const [resultado] = await conexion.execute(sql, [
-      fecha_reserva,
-      salon_id,
-      usuario_id,
-      turno_id,
-      foto_cumpleaniero,
-      tematica,
-      importe_salon,
-      importe_total,
-    ]);
-    if (resultado.affectedRows === 0) {
-      return null;
+    
+    //Browse: Obtiene todas las reservas
+    buscarTodasLasReservas = async () => {
+        const sql = 'SELECT * FROM reservas WHERE activo = 1';
+        const [reservas] = await conexion.execute(sql);
+        return reservas; 
     }
 
-    return this.buscarReserva(resultado.insertId);
-  };
+    //Read: Obtiene reserva por id
+    buscarReserva = async (reserva_id) => {
+        const sql = `SELECT * FROM reservas WHERE activo = 1 AND reserva_id = ?`;
+        const [reserva] = await conexion.execute(sql, [reserva_id]);
+        if(reserva.length === 0) {
+            return null;
+        }
+        return reserva[0]; 
+    }
+
+    //Add: Agrega nueva reserva
+    agregarReserva = async (reserva) => {
+        const {fecha_reserva, salon_id, usuario_id, turno_id, foto_cumpleaniero, tematica, importe_salon, importe_total} = reserva;
+        const sql = `INSERT INTO reservas (fecha_reserva, salon_id, usuario_id, turno_id, foto_cumpleaniero, tematica, importe_salon, importe_total) VALUES(?,?,?,?,?,?,?,?)`;
+        
+        const [resultado] = await conexion.execute(sql, [fecha_reserva, salon_id, usuario_id, turno_id, foto_cumpleaniero, tematica, importe_salon, importe_total]);
+        if(resultado.affectedRows === 0){
+            return null;
+        }
+
+        return this.buscarReserva(resultado.insertId);
+    }
+
+    //datos para notificacion despues...
 
     //Edit: modifica una reserva
     editarReserva = async (reserva_id, valores) => {
@@ -64,11 +43,14 @@ export default class Reservas {
         const sql = `UPDATE reservas
                         SET ${setValores}
                         WHERE reserva_id = ?`;
-
-    const [resultado] = await conexion.execute(sql, parametros);
-
-    if (resultado.affectedRows === 0) {
-      return null;
+        
+        const [resultado] = await conexion.execute(sql, parametros);
+        
+        if(resultado.affectedRows === 0) {
+            return null;
+        }
+        // retornamos la reserva actualizada
+        return this.buscarReserva(reserva_id);
     }
     
     //Delere: Eliminado lógico de reserva
@@ -76,9 +58,9 @@ export default class Reservas {
         const sql = `UPDATE reservas 
                         SET activo = 0
                         WHERE reserva_id = ?`;
-    const [reservaEliminada] = await conexion.execute(sql, [reserva_id]);
-    return reservaEliminada;
-  };
+        const [reservaEliminada] = await conexion.execute(sql, [reserva_id]);
+        return reservaEliminada;
+    }
 
     buscarDatosReporte = async () => {
         const sql = 'CALL sp_reporte_reservas_detalle()'; 
@@ -89,11 +71,11 @@ export default class Reservas {
     datosParaNotificacion = async (reserva_id) => {
         const sql = `CALL obtenerDatosNotificacion(?)`;
 
-    const [reserva] = await conexion.execute(sql, [reserva_id]);
-    if (reserva.length === 0) {
-      return null;
-    }
+        const [reserva] = await conexion.execute(sql, [reserva_id]);
+        if(reserva.length === 0){
+            return null;
+        }
 
-    return reserva;
-  };
+        return reserva;
+    }
 }
